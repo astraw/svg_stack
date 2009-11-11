bbc36dce5328e9b679347edd563ec169fc08a927
Andrew Straw <strawman@astraw.com>
2009-11-11 15:06:51 -0800

    add test for Inkscape svg file that fails without --inkscape option

--- run_tests.sh
+++ run_tests.sh
@@ -3,4 +3,5 @@ set -e
 python test1.py
 python test2.py
 python test3.py
+python test4.py
 echo 'OK'
---
+++ test4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import subprocess
+
+# stack two Inkscape generated files
+subprocess.check_call(
+    'svg_stack --direction=h --inkscape inkscape-pattern.svg > inkscape-pattern-copy.svg',
+    shell=True)
+
+# Inkscape files don't pass xmllint -- don't test
+
+print 'You should manually verify that inkscape-pattern.svg looks exactly the same as inkscape-pattern-copy.svg'
