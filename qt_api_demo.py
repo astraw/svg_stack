4d93a714348eb110983faeecbeb9921452d22b15
Andrew Straw <strawman@astraw.com>
2009-11-15 00:36:51 -0800

    partial support for alignment in Qt-like layout

--- qt_api_demo.py
+++ qt_api_demo.py
@@ -5,14 +5,14 @@ import svg_stack as ss
 doc = ss.Document()
 
 layout1 = ss.HBoxLayout()
-layout1.addSVG('red_ball.svg')
-layout1.addSVG('blue_triangle.svg')
+layout1.addSVG('red_ball.svg',alignment=ss.AlignTop|ss.AlignHCenter)
+layout1.addSVG('blue_triangle.svg',alignment=ss.AlignCenter)
 
 layout2 = ss.VBoxLayout()
 
-layout2.addSVG('red_ball.svg')
-layout2.addSVG('red_ball.svg')
-layout2.addSVG('red_ball.svg')
+layout2.addSVG('red_ball.svg',alignment=ss.AlignCenter)
+layout2.addSVG('red_ball.svg',alignment=ss.AlignCenter)
+layout2.addSVG('red_ball.svg',alignment=ss.AlignCenter)
 layout1.addLayout(layout2)
 
 doc.setLayout(layout1)
