ddaa6cd62fad810e5de5de43669895400d92d472
Bruno Souza <varnion@yandex.com>
2016-07-21 09:59:27 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='svg_stack',
-      version='0.0.1', # keep in sync with svg_stack.py
+      version='0.0.2', # keep in sync with svg_stack.py
       author='Andrew Straw <strawman@astraw.com>',
       url='http://github.com/astraw/svg_stack',
       py_modules=['svg_stack'],
