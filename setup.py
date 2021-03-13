2a8ec9b04092943aa3ccf9efe9f25cefebaa37fa
Andrew Straw <strawman@astraw.com>
2021-03-13 12:12:26 +0100

    add description to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 setup(name='svg_stack',
+      description='stack multiple SVG images into a single output',
       version='0.1.0', # keep in sync with svg_stack.py
       author='Andrew Straw <strawman@astraw.com>',
       url='http://github.com/astraw/svg_stack',
